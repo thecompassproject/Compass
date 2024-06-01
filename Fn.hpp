70c5740bdce1c7d9731c393fe214c86c2cb02c0d
SLang <spsinghlang@gmail.com>
2024-06-02 00:57:30 +0530

    Update Fn.hpp

--- Fn.hpp
+++ Fn.hpp
@@ -2,6 +2,6 @@
 
 template<typename Func>
 void fn(Func f, double& y, double& v, double t, double h) {
-    double y_derivative = f(t, y, v);
-    y += (h/2.0) * y_derivative;
+    double y_deri = f(t, y, v);
+    y += (h/2.0) * y_deri;
 }
