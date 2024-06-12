a6843c2d032ab3fe3477620261e5fc50fb7c3272
SLang <spsinghlang@gmail.com>
2024-06-13 02:15:05 +0530

    Update charge.cpp

--- charge.cpp
+++ charge.cpp
@@ -19,9 +19,8 @@ public:
     
         std::cout<<"::For numerical Method ::\n";
         std::cout<<"::    1 for Euler      ::\n";
-        std::cout<<"::     2 for Fn        ::\n";
-        std::cout<<"::   3 for Ralston     ::\n";
-        std::cout<<":: 4 for Runge-Kutta   ::\n";
+        std::cout<<"::   2 for Ralston     ::\n";
+        std::cout<<":: 3 for Runge-Kutta   ::\n";
         std::cout<<"::Enter the value selected from above::\n";
         std::cin>>val;     
          
@@ -56,18 +55,12 @@ for (double i = 0; i < n; i++) {
       x, y, z, vx, vy, vz, h);}
 
 else if(val == 2.0){
- fn3(std::bind(&field::AX, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
-      std::bind(&field::AY, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
-      std::bind(&field::AZ, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
-      x, y, z, vx, vy, vz, h);}
-
-else if(val == 3.0){
  ralston3(std::bind(&field::AX, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
       std::bind(&field::AY, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
       std::bind(&field::AZ, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
       x, y, z, vx, vy, vz, h);}
 
-else if(val == 4.0){
+else if(val == 3.0){
  rk43(std::bind(&field::AX, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
       std::bind(&field::AY, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
       std::bind(&field::AZ, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6),
