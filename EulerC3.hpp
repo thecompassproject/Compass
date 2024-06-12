5f5c5a738c372a449c3565742a053ebb4c8dfe71
SLang <spsinghlang@gmail.com>
2024-06-13 02:25:22 +0530

    Add files via upload

---
+++ EulerC3.hpp
@@ -0,0 +1,19 @@
+#include <functional>
+
+template<typename Func>
+void euler3(Func fx, Func fy, Func fz, double& x, double& y, double& z, double& vx, double& vy, double& vz, double h) {
+   
+    double dvx = h * fx(x, y, z, vx, vy, vz);
+    double dvy = h * fy(x, y, z, vx, vy, vz);
+    double dvz = h * fz(x, y, z, vx, vy, vz);
+
+   
+    x += h * vx;
+    y += h * vy;
+    z += h * vz;
+
+    vx += dvx;
+    vy += dvy;
+    vz += dvz;
+}
+
