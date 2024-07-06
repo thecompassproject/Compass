295216bfb705b5495a9333b692e69b6dad36e7ed
samreetdhillon <samreetsinghdhillon@github.com>
2024-07-06 21:05:37 +0530

    resolve conflicts

--- euler.hpp
+++ euler.hpp
@@ -1,6 +1,7 @@
-class euler{
-    public:
-        static double eulermethod(double x, double y, double z){
-            return (x + (y*z));
-        }
-};
+#include <functional>
+
+template<typename Func>
+void euler(Func f, double& y, double& v, double t, double h) {
+    double y_derivative = f(t, y, v);
+    y += h * y_derivative;
+}
--- fn.hpp
+++ fn.hpp
@@ -1,6 +1,7 @@
-class fn{
-    public:
-        static double fnmethod(double x, double y, double z){
-            return (x + (y*z/2));
-        }
-};
+#include <functional>
+
+template<typename Func>
+void fn(Func f, double& y, double& v, double t, double h) {
+    double y_derivative = f(t, y, v);
+    y += (h/2.0) * y_derivative;
+}
