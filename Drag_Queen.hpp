6929717d464b94ce56a602a5f643eb87f3c34b3a
SLang <spsinghlang@gmail.com>
2024-07-15 00:49:34 +0530

    Update Drag_Queen.hpp

--- Drag_Queen.hpp
+++ Drag_Queen.hpp
@@ -1,10 +1,10 @@
 class drag{
 private:
-    float r,p, g, m, c,mu,val;
+    float _r,_p, _g, _m, _c,_mu,_val,_x,_v;
     double A,B;
 public:
-drag(){
-    std::cout<< "Make sure the parameters should be in equivalent units\n";
+string input(double r,double p , double g, double m, double mu , double c , double val){
+    /*std::cout<< "Make sure the parameters should be in equivalent units\n";
     std::cout<< "\nEnter the radius of the sphere:::";
     std::cin>>r;
     std::cout<< "\nEnter the density of medium, Acceleration Due to Gravity, Mass of the sphere (respectively):::\n";
@@ -18,50 +18,57 @@ drag(){
     std::cout<<"::   2 for Ralston     ::\n";
     std::cout<<":: 3 for Runge-Kutta   ::\n";
     std::cout<<"::Enter the value selected from above::\n";
-    std::cin>>val;  
+    std::cin>>val;*/  
+_r = r; _p = p; 
+_g = g; _m = m; 
+_c = c; _mu = mu; 
+_val = val;
+     
 }
 
 float ono(double A,double v1,double B) {
-return g - (((p*4*atan(1)*c*r*r)/(2*m))*v1*v1)-((6*4*atan(1)*mu*r*v1)/m)-((4*p*r*r*r*4*atan(1)*g)/(3*m)) ;
+return _g - (((_p*4*atan(1)*_c*_r*_r)/(2*_m))*v1*v1)-((6*4*atan(1)*_mu*_r*v1)/_m)-((4*_p*_r*_r*_r*4*atan(1)*_g)/(3*_m)) ;
     }
 
 float yoko(double A, double x1 , double B1){
     return B1 ;
 }
 
-void b() { 
-    double in, x, v,a;
-    float h = 0.15 ; 
+void b(double x , double v) { 
+    double _a;
+    _x = x;
+    _v = v;
+    double h = 0.15 ; 
     A = 0; B = 0;
-    std::cout<< "\nEnter the initial value of Height:::";
+    /*std::cout<< "\nEnter the initial value of Height:::";
     std::cin>>x;
     std::cout<< "\nEnter the initial value of Velocity:::";
-    std::cin>>v;  
-    std::ofstream out("drag1.dat");
-    for (float t = 0.0; t > -1; t += h){
-        a = ono(A,v,B);
-        out<<t<<"  "<<x<<"  "<<v<<"  "<<a<<"\n";
-        if(val == 1.0){
-        eulersA(std::bind(&drag::yoko,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),x,v,A,-h);
-        eulersA(std::bind(&drag::ono,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),v,a,A,h);
+    std::cin>>v;*/  
+    std::ofstream out("drag.dat");
+    for (double t = 0.0; t > -1; t += h){
+        _a = ono(A,_v,B);
+        out<<t<<"  "<<_x<<"  "<<_v<<"  "<<_a<<"\n";
+        if(_val == 1.0){
+        eulersA(std::bind(&drag::yoko,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),_x,_v,A,-h);
+        eulersA(std::bind(&drag::ono,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),_v,_a,A,h);
         }
         
-       else if(val == 2.0){
-        ralsA(std::bind(&drag::yoko,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),x,v,A,-h);
-        ralsA(std::bind(&drag::ono,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),v,a,A,h);
+       else if(_val == 2.0){
+        ralsA(std::bind(&drag::yoko,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),_x,_v,A,-h);
+        ralsA(std::bind(&drag::ono,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),_v,_a,A,h);
         }
         
-       else if(val == 3.0){
-        rk4A(std::bind(&drag::yoko,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),x,v,A,-h);
-        rk4A(std::bind(&drag::ono,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),v,a,A,h);
+       else if(_val == 3.0){
+        rk4A(std::bind(&drag::yoko,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),_x,_v,A,-h);
+        rk4A(std::bind(&drag::ono,this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),_v,_a,A,h);
         }
         
         else{
-           std::cout<<"ERROR !!Not any Method!! \n";  
+           //std::cout<<"ERROR !!Not any Method!! \n";  
            break;
           }
         
-        if(x<0){
+        if(_x<0){
             break;
       }
     }
@@ -82,8 +89,8 @@ void b() {
         fprintf(gnuplotPipe, "set title 'Motion Of Spherical Body'\n");
         fprintf(gnuplotPipe, "set xlabel 'Time'\n");
         fprintf(gnuplotPipe, "set ylabel 'Height/Velocity/Acceleration'\n");
-        fprintf(gnuplotPipe, "plot 'drag1.dat' using 1:2 with lines title 'Height'\n");
-        fprintf(gnuplotPipe, "replot 'drag1.dat' using 1:3 with lines title 'Velocity'\n");
-        fprintf(gnuplotPipe, "replot 'drag1.dat' using 1:4 with lines title 'Acceleration'\n");
+        fprintf(gnuplotPipe, "plot 'drag.dat' using 1:2 with lines title 'Height'\n");
+        fprintf(gnuplotPipe, "replot 'drag.dat' using 1:3 with lines title 'Velocity'\n");
+        fprintf(gnuplotPipe, "replot 'drag.dat' using 1:4 with lines title 'Acceleration'\n");
   }
 };
