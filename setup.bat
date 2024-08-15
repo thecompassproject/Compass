aac1d66e1e13941718aa00f2cc27ac8056670b22
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2024-08-15 18:11:15 +0530

    feat: added setup.bat for setting up the project for first time

---
+++ setup.bat
@@ -0,0 +1,10 @@
+mkdir ThirdPartyLib
+cd ThirdPartyLib
+git clone https://github.com/pybind/pybind11.git 
+cd ..
+python m -venv venv
+venv/bin/activate.bat
+pip install -r requirements.txt
+build_script.bat
+cd website
+python manage.py runserver
