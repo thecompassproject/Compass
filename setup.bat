b6e6aa541e4faefa6a918b543d3eec51e02f6159
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2024-08-15 18:15:00 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@ mkdir ThirdPartyLib
 cd ThirdPartyLib
 git clone https://github.com/pybind/pybind11.git 
 cd ..
-python m -venv venv
+python -m venv venv
 venv/bin/activate.bat
 pip install -r requirements.txt
 build_script.bat
