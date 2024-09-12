d89c098cbf7c304371a51c95842adfcccf341fa1
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2024-09-12 23:42:09 +0530

    fix: copy static files while building exe

--- build_exe.bat
+++ build_exe.bat
@@ -1,3 +1,4 @@
 pyinstaller --name=compass website\start.py --copy-metadata django-bootstrap-v5 --noconfirm --add-data=website\algorithms\templates:algorithms\templates
 xcopy website\algorithms\programcodes dist\compass\algorithms\programcodes /e /i /h
+xcopy website\algorithms\static dist\compass\_internal\algorithms\static /e /i /h
 xcopy website\CodeBlocks dist\compass\CodeBlocks /e /i /h
